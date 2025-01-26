a5de0624196b55b4ce73306e7d631de6a7ba755e
Deepth1-253 <deepthibondalapati253@gmail.com>
2025-01-26 15:25:43 +0530

    Update App.java

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "test code change for github webhook1");
+        System.out.println( "test code change for github webhook");
     }
 }
